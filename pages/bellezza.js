17943f9c3fa3eaa10e273aa5beba15bf818a2bdb
BenedettaBottari <benedetta.bottari@studio.unibo.it>
2022-04-20 14:02:16 +0200

    modifiche grafiche pagine emozioni

--- pages/bellezza.js
+++ pages/bellezza.js
@@ -35,14 +35,14 @@ export default function Home() {
         <h1 className={style.title}>Bellezza</h1>
         <div className={style.player + ' d-flex'}>
             <Row className='w-100'>
-              <Col sm={5}>
+              <Col xs={5}>
                 <div className={style.shadow}>
                   <motion.div className={style.disc} variants={Variants} animate={isStart? "start" : "stop"}>
                     <img src={playlistImg[cont]}/>
                   </motion.div>
                 </div>
               </Col>
-              <Col sm={7}>
+              <Col xs={7}>
                 <h2 className="h4 mt-2 mx-1">{playlistTitle[cont]}</h2>
                 <p className="mx-4">{playlistArtist[cont]}</p>
                 <div className={style.btnPlayer + ' d-flex w-100 justify-content-center'}>
