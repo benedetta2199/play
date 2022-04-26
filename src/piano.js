09cec56b2444a4cd5a45948ee0ab0206807b56e7
BenedettaBottari <benedetta.bottari@studio.unibo.it>
2022-04-26 23:24:50 +0200

    add link piano

--- src/piano.js
+++ src/piano.js
@@ -13,26 +13,26 @@ function Piano() {
     return( 
         <div className={style.conteiner}>
             <ul className={style.piano + " set text-dark text-capitalize pl-5"}>      
-                <li className={classnames(style.white, style.start) + " rounded-top"}><p className={style.cAnsia}>ansia</p></li>
+                <Link href="/ansia"><li className={classnames(style.white, style.start) + " rounded-top"}><p className={style.cAnsia}>ansia</p></li></Link>
                     <li className={style.black} onClick={c}></li>
-                <li className={classnames(style.white, style.wb)}><p className={style.cBellezza}>bellezza</p></li>
+               <Link href="/bellezza"><li className={classnames(style.white, style.wb)}><p className={style.cBellezza}>bellezza</p></li></Link> 
                     <li className={style.black} onClick={d}></li>
-                <li className={classnames(style.white, style.wb)}><p className={style.cCalma}>calma</p></li>
+                <Link href="/calma"><li className={classnames(style.white, style.wb)}><p className={style.cCalma}>calma</p></li></Link>
                     <li className={style.black} onClick={f}></li>
-                <li className={classnames(style.white, style.wb)}><p className={style.cDivertimento}>divertimento</p></li>
-                <li className={style.white}><p className={style.cEnergia}>energia</p></li>
-                    <li className={style.black} onClick={g}></li>
+                <Link href="/divertimento"><li className={classnames(style.white, style.wb)}><p className={style.cDivertimento}>divertimento</p></li></Link>
+                <Link href="/energia"><li className={style.white}><p className={style.cEnergia}>energia</p></li></Link>
+                   <li className={style.black} onClick={g}></li>
                 <Link href="/fastidio"><li className={classnames(style.white, style.wb)}><p className={style.cFastidio}>fastidio</p></li></Link>
                     <li className={style.black} onClick={a}></li>
-                <li className={classnames(style.white, style.wb)}><p className={style.cGioia}>gioia</p></li>
+                <Link href="/gioia"><li className={classnames(style.white, style.wb)}><p className={style.cGioia}>gioia</p></li></Link>
                 <Link href="/passione"><li className={style.white}><p className={style.cPassione}>passione</p></li></Link>
                     <li className={style.black} onClick={c}></li>
-                <li className={classnames(style.white, style.wb)}><p className={style.cPaura}>paura</p></li>
+                <Link href="/paura"><li className={classnames(style.white, style.wb)}><p className={style.cPaura}>paura</p></li></Link>
                     <li className={style.black} onClick={d}></li>
                 <Link href="/sfida"><li className={classnames(style.white, style.wb)}><p className={style.cSfida}>sfida</p></li></Link>
                     <li className={style.black} onClick={f}></li>
-                <li className={classnames(style.white, style.wb)}><p className={style.cSogno}>sogno</p></li>
-                <li className={style.white}><p className={style.cTrionfo}>trionfo</p></li>
+                <Link href="/sogno"><li className={classnames(style.white, style.wb)}><p className={style.cSogno}>sogno</p></li></Link>
+                <Link href="/trionfo"><li className={style.white}><p className={style.cTrionfo}>trionfo</p></li></Link>
                     <li className={style.black} onClick={g}></li>
                 <Link href="/tristezza"><li className={classnames(style.white, style.wb) + " rounded-top"}><p className={style.cTristezza}>tristezza</p></li></Link>
             </ul>
