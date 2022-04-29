fb02e4c93b40406ce737463411e6b4d0c9627782
BenedettaBottari <benedetta.bottari@studio.unibo.it>
2022-04-29 11:32:18 +0200

    add infografic (warning) 1

--- pages/vinil-radar.js
+++ pages/vinil-radar.js
@@ -13,24 +13,26 @@ export default function VinilRadar(props) {
   const spotify = <iframe src={"https://open.spotify.com/embed/track/"+id} style={{borderRadius:'10px', boxShadow:'0 0 15px #000'}}
         width="180" height="80" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"/>
 
-  dte.forEach(element => {
-    switch(element){
-      case 'Ansia': emotion.push('   Ansia    '); color.push('#fc1716'); bg.push('#fc171690'); break
-      case 'Bellezza': emotion.push('  Bellezza  '); color.push('#FE266D'); bg.push('#FE266D90'); break
-      case 'Calma': emotion.push('   Calma    '); color.push('#ff47bc'); bg.push('#ff47bc90'); break
-      case 'Divertimento': emotion.push('Divertimento'); color.push('#ff7667'); bg.push('#ff766790'); break
-      case 'Energia': emotion.push('  Energia   '); color.push('#ff8a43'); bg.push('#ff8a4390'); break
-      case 'Fastidio': emotion.push('  Fastidio  '); color.push('#ffcf1a'); bg.push('#ffcf1a90'); break
-      case 'Gioia': emotion.push('   Gioia    '); color.push('#FAFF07'); bg.push('#FAFF0790'); break
-      case 'Passione': emotion.push('  Passione  '); color.push('#adfc21'); bg.push('#adfc2190'); break
-      case 'Paura': emotion.push('   Paura    '); color.push('#44ff41'); bg.push('#44ff4190'); break
-      case 'Sfida': emotion.push('   Sfida    '); color.push('#29f887'); bg.push('#29f88790'); break
-      case 'Sogno': emotion.push('   Sogno    '); color.push('#0ff0eb'); bg.push('#0ff0eb90'); break
-      case 'Trionfo': emotion.push('  Trionfo   '); color.push('#6554ff'); bg.push('#6554ff90'); break
-      case 'Tristezza': emotion.push(' Tristezza  '); color.push('#bb17ff'); bg.push('#bb17ff90'); break
-      
-    }
-  });
+  if(dte){
+    dte.forEach(element => {
+      switch(element){
+        case 'Ansia': emotion.push('   Ansia    '); color.push('#fc1716'); bg.push('#fc171690'); break
+        case 'Bellezza': emotion.push('  Bellezza  '); color.push('#FE266D'); bg.push('#FE266D90'); break
+        case 'Calma': emotion.push('   Calma    '); color.push('#ff47bc'); bg.push('#ff47bc90'); break
+        case 'Divertimento': emotion.push('Divertimento'); color.push('#ff7667'); bg.push('#ff766790'); break
+        case 'Energia': emotion.push('  Energia   '); color.push('#ff8a43'); bg.push('#ff8a4390'); break
+        case 'Fastidio': emotion.push('  Fastidio  '); color.push('#ffcf1a'); bg.push('#ffcf1a90'); break
+        case 'Gioia': emotion.push('   Gioia    '); color.push('#FAFF07'); bg.push('#FAFF0790'); break
+        case 'Passione': emotion.push('  Passione  '); color.push('#adfc21'); bg.push('#adfc2190'); break
+        case 'Paura': emotion.push('   Paura    '); color.push('#44ff41'); bg.push('#44ff4190'); break
+        case 'Sfida': emotion.push('   Sfida    '); color.push('#29f887'); bg.push('#29f88790'); break
+        case 'Sogno': emotion.push('   Sogno    '); color.push('#0ff0eb'); bg.push('#0ff0eb90'); break
+        case 'Trionfo': emotion.push('  Trionfo   '); color.push('#6554ff'); bg.push('#6554ff90'); break
+        case 'Tristezza': emotion.push(' Tristezza  '); color.push('#bb17ff'); bg.push('#bb17ff90'); break
+        
+      }
+    });
+  }
 
   ChartJS.register(RadialLinearScale,PointElement,LineElement,Filler,Tooltip,Legend);
 const data = {
