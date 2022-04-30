ba5bd149c1c460d15db20a41494bc253e00bc6fb
BenedettaBottari <benedetta.bottari@studio.unibo.it>
2022-04-30 11:56:57 +0200

    add infografic

--- pages/disk.js
+++ pages/disk.js
@@ -5,53 +5,35 @@ import style from '../style/idea.module.css';
 import { SkipBackwardFill, SkipForwardFill, ChevronDoubleDown, ChevronDoubleRight } from 'react-bootstrap-icons';
 import Helmet from 'react-helmet';
 import VinilRadar from './vinil-radar';
+import { useState } from 'react';
 
-export default function Disk(props) {
-
-  const {index} = props;
-  const cd= [];
-  var cont = 0, i = 0;
-
+export default function Disk() {
+  const [dts, setDts] =useState([0,0,0,0,0,0,0,0,0,0,0,0,0]);
+  const [dte, setDte] = useState(['A','B','C','D','E','F','G','P','Pa','Sf','So','T','Tr']);
+  const [title, setTitle]=useState('');
+  
   const disk = [
-    {dts:[31,25,5,26,9], dte:['Trionfo','Ansia','Bellezza','Energia','Sfida'], title:'Eye of Tyger', id:"2KH16WveTQWT6KOG9Rg6e2"},
-    {dts:[24,16,22,20,18], dte:['Bellezza','Calma','Energia','Sogno','Trionfo'], title:'Bitter Sweet Symphony', id:"57iDDD9N9tTWe75x6qhStw"},
-    {dts:[31,15,9,26,18], dte:['Ansia','Energia','Fastidio','Paura','Trionfo'], title:'The Rite of Spring', id:"5MSHxf0zVSajZknoxJC9Nj"},
-    {dts:[34,19,10,17,18], dte:['Passione','Sogno','Bellezza','Calma','Divertimento'], title:'Human Nature', id:"35lAjvsvS9k9HHuP1fjDT8"},
-    {dts:[31,28,23,11,9], dte:['Tristezza','Bellezza','Calma','Passione','Sogno'], title:'Summertime Sadness', id:"33CeM8NI7tfrNgciVOFMoo"},
-    {dts:[27,12,15,17,23], dte:['Calma','Energia','Passione','Sogno','Bellezza'], title:"How Far I'll Go", id:"55dS7JjavLsf63BynwsbV5"},
-    {dts:[34,30,13,8,14], dte:['Bellezza','Calma','Energia','Gioia','Sogno'], title:'Mykonos', id:"7H71gZwWs3xPWnHM30NDUp"},
-    {dts:[46,6,9,20,14], dte:['Calma','Passione','Sogno','Tristezza','Bellezza'], title:'Paris', id:"15vzANxN8G9wWfwAJLLMCg"},
-    {dts:[26,17,14,17,14], dte:['Gioia','Passione','Tristezza','Calma','Energia'], title:'The Scientist', id:"75JFxkI2RXiU7L9VXzMkle"},
-    {dts:[33,6,21,8,31], dte:['Fastidio','Paura','Sfida','Ansia','Energia'], title:'Divide & Conquer', id:"3HD05YfZS6L8ZZD0Gl47Hq"},
-    {dts:[42,22,10,8,14], dte:['Energia','Fastidio','Sfida','Trionfo','Ansia'], title:'Run the World', id:"1uXbwHHfgsXcUKfSZw5ZJ0"},
-
+    {dts:[31,0,24,5,0,0,26,0,0,0,0,9,0], title:'Eye of Tyger', id:"2KH16WveTQWT6KOG9Rg6e2", dte:['T','Tr','A','B','C','D','E','F','G','P','Pa','Sf','So']},
+    {dts:[24,16,0,22,0,0,0,0,0,20,18,0,0], title:'Bitter Sweet Symphony', id:"57iDDD9N9tTWe75x6qhStw", dte: ['B','C','D','E','F','G','P','Pa','Sf','So','T','Tr','A']},
+    {dts:[31,0,0,0,15,9,0,0,26,0,0,18,0], title:'The Rite of Spring', id:"5MSHxf0zVSajZknoxJC9Nj",dte:['A','B','C','D','E','F','G','P','Pa','Sf','So','T','Tr']},
+    {dts:[34,0,0,19,0,0,0,10,17,18,0,0,0], title:'Human Nature', id:"35lAjvsvS9k9HHuP1fjDT8", dte:['P','Pa','Sf','So','T','Tr','A','B','C','D','E','F','G']},
+    {dts:[31,0,28,23,0,0,0,0,11,0,0,9,0], title:'Summertime Sadness', id:"33CeM8NI7tfrNgciVOFMoo", dte:['Tr','A','B','C','D','E','F','G','P','Pa','Sf','So','T']},
+    {dts:[27,0,12,0,0,15,0,0,17,0,0,0,23], title:"How Far I'll Go", id:"55dS7JjavLsf63BynwsbV5", dte:['C','D','E','F','G','P','Pa','Sf','So','T','Tr','A','B']},
+    {dts:[34,30,0,13,0,8,0,0,0,14,0,0,0], title:'Mykonos', id:"7H71gZwWs3xPWnHM30NDUp", dte:['B','C','D','E','F','G','P','Pa','Sf','So','T','Tr','A']},
+    {dts:[46,0,0,0,0,6,0,0,9,0,20,0,14], title:'Paris', id:"15vzANxN8G9wWfwAJLLMCg", dte:['C','D','E','F','G','P','Pa','Sf','So','T','Tr','A','B']},
+    {dts:[26,17,0,0,0,0,14,0,0,17,0,14,0], title:'The Scientist', id:"75JFxkI2RXiU7L9VXzMkle", dte:['G','P','Pa','Sf','So','T','Tr','A','B','C','D','E','F']},
+    {dts:[33,0,0,6,21,0,0,0,8,0,0,0,31], title:'Divide & Conquer', id:"3HD05YfZS6L8ZZD0Gl47Hq", dte:['F','G','P','Pa','Sf','So','T','Tr','A','B','C','D','E']},
+    {dts:[42,22,0,0,0,10,0,8,0,14,0,0,0], title:'Run the World', id:"1uXbwHHfgsXcUKfSZw5ZJ0", dte:['E','F','G','P','Pa','Sf','So','T','Tr','A','B','C','D']},
   ]
 
-  {
-    /*
-    for(var i=0; i<5; i++){
-
-    const n = Math.floor(Math.random() * disk.length)%2;
-    if(index.includes(n)){
-      i--;
-    } else{
-      cd.push(<VinilRadar dts={disk[i].dts} dte={disk[n].dte} title={disk[n].title} id={disk[n].id} key={i}/>);
-    }
-  }
-  while(cont>5 || i>=disk.length){
-    if(parseInt(Math.random())%2==0){
-      cd.push(<VinilRadar dts={disk[i].dts} dte={disk[n].dte} title={disk[n].title} id={disk[n].id} key={i}/>);
-      cont++;
-    }
-    i++
-  }*/
-}
-
   return (
-    
       <div className='d-flex flex-wrap justify-content-center pt-4'>
-        {disk.map(e=>{
-          <iframe src={"https://open.spotify.com/embed/track/"+e.id} style={{borderRadius:'10px', boxShadow:'0 0 15px #000'}} width="80" height="80"/>
+        {disk.map((e,i)=>{
+          return(<div className='d-flex flex-column justify-content-center px-2' key={i}>
+            <iframe src={"https://open.spotify.com/embed/track/"+e.id} style={{borderRadius:'10px', boxShadow:'0 0 15px #000'}} width="80" height="80"/>
+            <Button className='btn-sm mt-2 py-1 btn-secondary' 
+            onClick={()=>{setDte(e.dte);setDts(e.dts);setTitle(title);}}>Dati</Button>
+          </div>)
         })}
         <VinilRadar dts={dts} dte={dte} title={title}/>
       </div>
--- pages/vinil-radar.js
+++ pages/vinil-radar.js
@@ -6,7 +6,7 @@ import {Chart as ChartJS, RadialLinearScale,PointElement,LineElement,Filler,Tool
 import { Radar } from 'react-chartjs-2';
 
 export default function VinilRadar(props) {
-  const {dts, dte, title, id, v} = props;
+  const {dts, dte, title} = props;
   const color = [];
   const bg = [];
   const emotion = [];
@@ -14,21 +14,20 @@ export default function VinilRadar(props) {
   if(dte){
     dte.forEach(element => {
       switch(element){
-        case 'Ansia': emotion.push('   Ansia    '); color.push('#fc1716'); bg.push('#fc171690'); break
-        case 'Bellezza': emotion.push('  Bellezza  '); color.push('#FE266D'); bg.push('#FE266D90'); break
-        case 'Calma': emotion.push('   Calma    '); color.push('#ff47bc'); bg.push('#ff47bc90'); break
-        case 'Divertimento': emotion.push('Divertimento'); color.push('#ff7667'); bg.push('#ff766790'); break
-        case 'Energia': emotion.push('  Energia   '); color.push('#ff8a43'); bg.push('#ff8a4390'); break
-        case 'Fastidio': emotion.push('  Fastidio  '); color.push('#ffcf1a'); bg.push('#ffcf1a90'); break
-        case 'Gioia': emotion.push('   Gioia    '); color.push('#FAFF07'); bg.push('#FAFF0790'); break
-        case 'Passione': emotion.push('  Passione  '); color.push('#adfc21'); bg.push('#adfc2190'); break
-        case 'Paura': emotion.push('   Paura    '); color.push('#44ff41'); bg.push('#44ff4190'); break
-        case 'Sfida': emotion.push('   Sfida    '); color.push('#29f887'); bg.push('#29f88790'); break
-        case 'Sogno': emotion.push('   Sogno    '); color.push('#0ff0eb'); bg.push('#0ff0eb90'); break
-        case 'Trionfo': emotion.push('  Trionfo   '); color.push('#6554ff'); bg.push('#6554ff90'); break
-        case 'Tristezza': emotion.push(' Tristezza  '); color.push('#bb17ff'); bg.push('#bb17ff90'); break
-      }
-    });
+        case 'A': emotion.push('   Ansia    '); color.push('#fc1716'); bg.push('#fc171690'); break
+        case 'B': emotion.push('  Bellezza  '); color.push('#FE266D'); bg.push('#FE266D90'); break
+        case 'C': emotion.push('   Calma    '); color.push('#ff47bc'); bg.push('#ff47bc90'); break
+        case 'D': emotion.push('Divertimento'); color.push('#ff7667'); bg.push('#ff766790'); break
+        case 'E': emotion.push('  Energia   '); color.push('#ff8a43'); bg.push('#ff8a4390'); break
+        case 'F': emotion.push('  Fastidio  '); color.push('#ffcf1a'); bg.push('#ffcf1a90'); break
+        case 'G': emotion.push('   Gioia    '); color.push('#FAFF07'); bg.push('#FAFF0790'); break
+        case 'P': emotion.push('  Passione  '); color.push('#adfc21'); bg.push('#adfc2190'); break
+        case 'Pa': emotion.push('   Paura    '); color.push('#44ff41'); bg.push('#44ff4190'); break
+        case 'Sf': emotion.push('   Sfida    '); color.push('#29f887'); bg.push('#29f88790'); break
+        case 'So': emotion.push('   Sogno    '); color.push('#0ff0eb'); bg.push('#0ff0eb90'); break
+        case 'T': emotion.push('  Trionfo   '); color.push('#6554ff'); bg.push('#6554ff90'); break
+        case 'Tr': emotion.push(' Tristezza  '); color.push('#bb17ff'); bg.push('#bb17ff90'); break
+      }});
   }
 
   ChartJS.register(RadialLinearScale,PointElement,LineElement,Filler,Tooltip,Legend);
@@ -52,11 +51,11 @@ const options= {
     grid:{color:'black', circular:true},
     pointLabels:{
       color: color,
-      font: {family:'Prompt',weight:'bold'},
+      font: {family:'Prompt',weight:'bold',size:'12px'},
       width: '100px'
     },
     ticks:{display:false},
-    min: 0, max: 50
+    min: 0, max: 50, stepSize: 10
     }
   },
   plugins: {
@@ -71,19 +70,10 @@ const options= {
   }
 };
   return (
-    <>
-      <div className={v?'':'invisible '+'px-5 mx-2'} style={{width:'390px'}}>
-        <div className='d-flex flex-wrap justify-content-start' style={{height:'80px',}}>        
-          <h2 className='h6 m-0 px-2 py-3'>{title}</h2>
-          </div>
-      </div>
-      {
-        v && (
-          <div style={{width:"340px",height:"350px", position:'relative', top:'-85px'}}>
-              <Radar data={data} options={options} 
-              style={{backgroundImage:"url(./disk.png)",backgroundSize:'contain',backgroundPosition: "center",backgroundRepeat: "no-repeat"}}/>
-          </div>)
-      }
-    </>
+    <div style={{width:"500px",height:"500px"}}>
+        <h2 className='text-light display-1'>{title}</h2>
+        <Radar data={data} options={options} 
+        style={{backgroundImage:"url(./disk.png)",backgroundSize:'cover',backgroundPosition: "center",backgroundRepeat: "no-repeat"}}/>
+    </div>
   )
 }
