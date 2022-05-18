801ec7127fdc7e1428ec44b6667e44d6088cf9c3
BenedettaBottari <benedetta.bottari@studio.unibo.it>
2022-05-18 15:53:32 +0200

    sistemzioni errori 2

--- pages/emotion-temp.js
+++ pages/emotion-temp.js
@@ -12,33 +12,31 @@ import emailjs from 'emailjs-com'
 
 export default function Emotion(props) {
 
-  /*var tracks=[];
-  switch(titolo){
-    case "ansia":tracks=[useSound('./music/'+name+'/1.mp3'),useSound('./music/'+name+'/2.mp3'),useSound('./music/'+name+'/3.mp3')]; break;
-    case "bellezza":tracks=[useSound('./music/'+name+'/1.mp3'),useSound('./music/'+name+'/2.mp3'),useSound('./music/'+name+'/3.mp3')]break;
-    case "calma":tracks=[useSound('./music/'+name+'/1.mp3'),useSound('./music/'+name+'/2.mp3'),useSound('./music/'+name+'/3.mp3')] break;
-    case "divertimento":tracks=[useSound('./music/'+name+'/1.mp3'),useSound('./music/'+name+'/2.mp3'),useSound('./music/'+name+'/3.mp3')]break;        
-    case "energia":tracks=[useSound('./music/'+name+'/1.mp3'),useSound('./music/'+name+'/2.mp3'),useSound('./music/'+name+'/3.mp3')]break;    
-    case "fastidio":tracks=[useSound('./music/'+name+'/1.mp3'),useSound('./music/'+name+'/2.mp3'),useSound('./music/'+name+'/3.mp3')]break;
-    case "gioia":tracks=[useSound('./music/'+name+'/1.mp3'),useSound('./music/'+name+'/2.mp3'),useSound('./music/'+name+'/3.mp3')]break;
-    case "passione":break;
-    case "paura":break;
-    case "sfida":break;
-    case "sogno": break;
-    case "trionfo":break;
-    case "tristezza": break;
-  }*/
-
   const {title, linkImg, st, color, card, cit, Acit, idSpotify, t, a, trak, img, n} = props;
-  var inputTitle;
+  var tracks=[];
+    switch(title){
+      case "ansia":tracks=[useSound('./music/'+title+'/1.mp3'),useSound('./music/'+title+'/2.mp3'),useSound('./music/'+title+'/3.mp3')]; break;
+      case "bellezza":tracks=[useSound('./music/'+title+'/1.mp3'),useSound('./music/'+title+'/2.mp3'),useSound('./music/'+title+'/3.mp3')];break;
+      case "calma":tracks=[useSound('./music/'+title+'/1.mp3'),useSound('./music/'+title+'/2.mp3'),useSound('./music/'+title+'/3.mp3')]; break;
+      case "divertimento":tracks=[useSound('./music/'+title+'/1.mp3')];break;        
+      case "energia":tracks=[useSound('./music/'+title+'/1.mp3'),useSound('./music/'+title+'/2.mp3')];break;    
+      case "fastidio":tracks=[useSound('./music/'+title+'/1.mp3'),useSound('./music/'+title+'/2.mp3')];break;
+      case "gioia":tracks=[useSound('./music/'+title+'/1.mp3')];break;
+      case "passione":tracks=[useSound('./music/'+title+'/1.mp3'),useSound('./music/'+title+'/2.mp3'),useSound('./music/'+title+'/3.mp3'),useSound('./music/'+title+'/4.mp3')];break;
+      case "paura":tracks=[useSound('./music/'+title+'/1.mp3'),useSound('./music/'+title+'/2.mp3'),useSound('./music/'+title+'/3.mp3')];break;
+      case "sfida":tracks=[useSound('./music/'+title+'/1.mp3'),useSound('./music/'+title+'/2.mp3'),useSound('./music/'+title+'/3.mp3')];break;
+      case "sogno":tracks=[useSound('./music/'+title+'/1.mp3'),useSound('./music/'+title+'/2.mp3'),useSound('./music/'+title+'/3.mp3')];break;
+      case "trionfo":tracks=[useSound('./music/'+title+'/1.mp3'),useSound('./music/'+title+'/2.mp3')];break;
+      case "tristezza":tracks=[useSound('./music/'+title+'/1.mp3'),useSound('./music/'+title+'/2.mp3')];break;
+    }
 
   const controls = useAnimation();
-  const tracks = trak;
+  /*const tracks = trak;
   const track = new Array(n);
   for(var i=0; i<n; i++){
     track.push(useSound('./music/'+title+'/'+(i+1)+'.mp3'));
     console.log(i+1);
-  }
+  }*/
   const titles=t;
   const artists=a;
   const imgs=img;
@@ -48,8 +46,8 @@ export default function Emotion(props) {
     };
     
     const [isStart, setIsStart] = useState(false);
-    const lenght = n;
-    console.log(lenght)
+    const lenght = tracks.length;
+    console.log(lenght);
     
     const [cont, setCont] = useState(0);  
     
@@ -69,30 +67,30 @@ export default function Emotion(props) {
     }
     
   return (
-    <div className="container">
+    <div classtitle="container">
 
-      <div className={style.divHead + " imgCredit"}>
-        <img src={"./emotion/"+title+".jpg"} className={style.imgHead} alt=""/>
-        <a href={linkImg} className={color + " credit"} target="_blank">created by master1305 - www.freepik.com</a>
+      <div classtitle={style.divHead + " imgCredit"}>
+        <img src={"./emotion/"+title+".jpg"} classtitle={style.imgHead} alt=""/>
+        <a href={linkImg} classtitle={color + " credit"} target="_blank">created by master1305 - www.freepik.com</a>
       </div>
-      <section className={style.sectionOne}>
-        <h1 className={style.title}>{title}</h1>
-        <div className={style.player + ' d-flex'}>
-            <Row className='w-100'>
+      <section classtitle={style.sectionOne}>
+        <h1 classtitle={style.title}>{title}</h1>
+        <div classtitle={style.player + ' d-flex'}>
+            <Row classtitle='w-100'>
               <Col xs={5}>
-                <div className={style.shadow}>
-                  <motion.div className={style.disc} animate={controls} variants={Variants}>
+                <div classtitle={style.shadow}>
+                  <motion.div classtitle={style.disc} animate={controls} variants={Variants}>
                     {/*imgs && imgs[cont]} alt={"immagine profilo di " + artists[cont]}*/}
                     <img src='/logo.png'/>
                   </motion.div>
                 </div>
               </Col>
               <Col xs={7}>
-                <h2 className="h4 mt-md-3 mt-4 mx-2">{/*titles && titles[cont]*/}</h2>
-                <p className="mx-4">{/*artists && artists[cont]*/}</p>
-                <div className={style.btnPlayer + ' d-flex w-100 justify-content-center'}>
+                <h2 classtitle="h4 mt-md-3 mt-4 mx-2">{/*titles && titles[cont]*/}</h2>
+                <p classtitle="mx-4">{/*artists && artists[cont]*/}</p>
+                <div classtitle={style.btnPlayer + ' d-flex w-100 justify-content-center'}>
                   <SkipBackwardFill onClick={()=>{setCont((cont+lenght-1)%lenght); stop(); setIsStart(false); controls.start("stop"); setButton(<PlayFill/>)}}/>
-                  <a className='m-0 p-0'
+                  <a classtitle='m-0 p-0'
                     onClick={()=>{setIsStart(!isStart);
                                   if(isStart){pause(); controls.stop(); setButton(<PlayFill/>);}
                                   else {play(); controls.start("start"); setButton(<PauseFill/>)}}}>
@@ -104,38 +102,38 @@ export default function Emotion(props) {
             </Row>        
         </div>
           
-        <Link to="more" spy={true}  smooth={true} offset={-100} duration={500} className={st+" btn btn-outline btn-outline-light m-5 my-4"}>
-          <ChevronDoubleDown/><span className="mx-3">Ascolta di più</span>
+        <Link to="more" spy={true}  smooth={true} offset={-100} duration={500} classtitle={st+" btn btn-outline btn-outline-light m-5 my-4"}>
+          <ChevronDoubleDown/><span classtitle="mx-3">Ascolta di più</span>
         </Link>
 
           <form onSubmit={sendEmail} class={style.consigli + ' d-flex flex-column'}>
             <label for="message">Consigliaci altri brani:</label>
-            <input type="hidden" name="title" id="title" value={title}/>
-            <input type="text" name="message" id="message" class="text-dark" ref={e => inputTitle = e}/>
-            <div className='text-md-end text-center'>
-              <input type="submit" value="Invia" className={st+' btn btn-sm border-light btn-outline text-light'}/>
+            <input type="hidden" title="title" id="title" value={title}/>
+            <input type="text" title="message" id="message" class="text-dark" ref={e => inputTitle = e}/>
+            <div classtitle='text-md-end text-center'>
+              <input type="submit" value="Invia" classtitle={st+' btn btn-sm border-light btn-outline text-light'}/>
             </div>
           </form>
         </section>
         
-        {/*<section className={style.Spotify + ' text-center'}>
+        {/*<section classtitle={style.Spotify + ' text-center'}>
           <h2>Ascolta anche:</h2>
-          <div className="d-flex flex-wrap justify-content-around">
+          <div classtitle="d-flex flex-wrap justify-content-around">
             {card && card.map (element => <CardEmotion titolo={element} key={element}/>)}
           </div>
         </section>*/}
 
         <section>
-            <Row className='mb-5 pb-3 text-center'>
-                <p className='m-0'>{cit}</p>
-                <p><em className='font-italic'>{Acit}</em></p>
+            <Row classtitle='mb-5 pb-3 text-center'>
+                <p classtitle='m-0'>{cit}</p>
+                <p><em classtitle='font-italic'>{Acit}</em></p>
             </Row>
             <Row  id="more">
-                <Col md={6} className='d-flex align-items-center'>
-                    <h2 className='display-2 px-4'>Ascolta le tue emozioni anche su Spotify</h2>
+                <Col md={6} classtitle='d-flex align-items-center'>
+                    <h2 classtitle='display-2 px-4'>Ascolta le tue emozioni anche su Spotify</h2>
                 </Col>
-                <Col md={6} className="text-center">
-                  <iframe className={style.plSpotify}  src={"https://open.spotify.com/embed/playlist/"+idSpotify+"?utm_source=generator" }
+                <Col md={6} classtitle="text-center">
+                  <iframe classtitle={style.plSpotify}  src={"https://open.spotify.com/embed/playlist/"+idSpotify+"?utm_source=generator" }
                   width="350" height="380" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
         
                 </Col>
