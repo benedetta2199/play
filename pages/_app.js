d5881631e3882d974658ec026863613a188e5595
BenedettaBottari <benedetta.bottari@studio.unibo.it>
2022-05-20 18:57:59 +0200

    correzione errori google analitics

--- pages/_app.js
+++ pages/_app.js
@@ -27,14 +27,14 @@ function MyApp({ Component, pageProps }) {
   
   return (
     <>
-      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
 
       <Script strategy="lazyOnload">
           {`
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              gtag('config', '${NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
               page_path: window.location.pathname,
               });
           `}
